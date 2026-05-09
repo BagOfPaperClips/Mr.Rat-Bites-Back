21700d1fed8fa55a8a420a6bb06b684056d6424d
BagOfPaperClips <samrgenovese@gmail.com>
2026-05-09 19:05:07 -0400

    Pause Menu added

---
+++ Mr.Rat Bites Back/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    public GameObject gamePause;
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter == 0 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Hello!?");
+            
+            gamePause.SetActive(true);
+            Time.timeScale = 0;
+            StartCoroutine(back());
+        }
+        if(counter==1 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Bye!!");
+            gamePause.SetActive(false);
+            Time.timeScale = 1;
+            StartCoroutine(back());
+
+        }
+    }
+    IEnumerator back()
+    {
+        Debug.Log(counter);
+        yield return new WaitForSecondsRealtime(0.1f);
+        if (counter == 1)
+            counter = 0;
+        else if (counter==0)
+            counter = 1;
+        
+    }
+
+    public void Pause()
+    {
+        gamePause.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
